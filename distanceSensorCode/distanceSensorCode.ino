0fbb390269978f2499eb1f4eaeb830e314f3a724
skakanuru <kakanuru.sathvik@gmail.com>
2024-02-06 14:35:53 -0600

    Initial Commit

---
+++ distanceSensorCode/distanceSensorCode.ino
@@ -0,0 +1,20 @@
+const int trigPin = A;
+const int echoPin = B;
+long duration;
+int distanceCm, distanceInch;
+
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(trigPin, OUTPUT);
+  pinMode(echoPin, INPUT);
+  Serial.begin(9600);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  digitalWrite(trigPin, LOW);
+  delayMicroseconds(2);
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigPin, LOW); 
+}
